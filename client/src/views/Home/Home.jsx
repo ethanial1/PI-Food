068f661d25a5f59298a291d8e133964c38ff0848
ethanial1 <camiloeiz00@gmail.com>
2022-02-09 10:25:46 -0600

    estilos de home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import AllRecipes from '../../components/Recetas/AllRecipes';
+import st from './Home.module.css'
+import imgIzqu from '../../assets/izquierda.svg'
+import imgDerch from '../../assets/derecha.svg'
 
 const Home = () => {
   return (
-    <>
-    <NavBar />
-    <AllRecipes />
-    </>
+    <div className={st.home}>
+      <img src={imgIzqu} alt="izquierda" className={st.img_izq}/>
+      <NavBar />
+      <AllRecipes />
+      <img src={imgDerch} alt="derecha" className={st.img_der}/>
+    </div>
   )
 };
 
