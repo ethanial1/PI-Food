d3c1601a73ff908c85f2b2a191368910413375fa
ethanial1 <camiloeiz00@gmail.com>
2022-02-09 21:48:10 -0600

    estilos de formulario

--- client/src/components/Recetas/FormCreate.jsx
+++ client/src/components/Recetas/FormCreate.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import st from './FormCreate.module.css'
+import img from '../../assets/formImg.jpg'
 
 const FormCreate = () => {
   return (
-    <section>
-      <div>
+    <section className={st.section}>
+      <div className={st.img}>
+        <img src={img} alt="" />
+      </div>
+      <div className={st.form}>
         <form>
-          <div>
-            <label htmlFor="nombre">Nombre:</label>
-            <input type="text" name="nombre" id="nombre" />
+          <div className={st.group}>
+            <label htmlFor="nombre">Nombre <span>*</span></label>
+            <input type="text" name="nombre" id="nombre" placeholder='Mi receta favorita'/>
           </div>
-          <div>
-            <label htmlFor="resumen">Resumen del plato</label>
-            <textarea name="resumen" id="resumen" cols="30" rows="10"></textarea>
+          <div className={st.group}>
+            <label htmlFor="resumen">Resumen del plato <span>*</span></label>
+            <textarea name="resumen" id="resumen" cols="30" rows="4" placeholder=''></textarea>
           </div>
-          <div>
+          <div className={st.group}>
             <label htmlFor="puntuacion">Puntuación</label>
             <input type="number" name="puntuacion" id="puntuacion" />
           </div>
-          <div>
-            <label htmlFor="saludable">Nivel de comida saludable</label>
+          <div className={st.group}>
+            <label htmlFor="saludable">Nivel de comida saludable <span>*</span></label>
             <input type="range" name="saludable" id="saludable" />
           </div>
-          <div>
+          <div className={st.group}>
             <label htmlFor="pasos">Pasos</label>
-            <textarea name="pasos" id="pasos" cols="30" rows="10"></textarea>
+            <textarea name="pasos" id="pasos" cols="30" rows="5"></textarea>
           </div>
-          <div>
-            <label htmlFor="dietas">Dietas</label>
+          <div className={st.group}>
+            <label htmlFor="dietas">Dietas <span>*</span></label>
           </div>
           <button type="submit">Guardar</button>
         </form>
