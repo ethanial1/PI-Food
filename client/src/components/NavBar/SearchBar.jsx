30e435ba7a1794275a783c91471e6a1161930c0d
ethanial1 <camiloeiz00@gmail.com>
2022-02-11 10:48:07 -0600

    validaciones searchbar

--- client/src/components/NavBar/SearchBar.jsx
+++ client/src/components/NavBar/SearchBar.jsx
@@ -3,20 +3,33 @@ import { useDispatch } from 'react-redux';
 import { getRecipesByName } from '../../Redux/Actions/actions';
 import st from './NavBar.module.css';
 
+const validate = nombre => {
+    const err = {}
+
+    if(!nombre.trim()) {
+        err.nombre = 'Nombre de receta requerido'
+    }
+
+    return err
+}
+
 const SearchBar = () => {
     const [nombre, setNombre] = useState('');
     const [errores, setErrores] = useState({});
     const dispatch = useDispatch()
 
+    const handleChange = e => {
+        setNombre(e.target.value)
+        setErrores(validate(nombre))
+    }
+
+    const handleBlur = nombre => {
+        setErrores(validate(nombre))
+    } 
 
     const handleSubmit = e => {
         e.preventDefault()
-
-        if(!nombre.trim()) {
-            setErrores({nombre:"Nombre de receta requerido"})
-            return
-        }
-
+        setErrores(validate(nombre))
         if(Object.keys(errores).length === 0) {
             dispatch(getRecipesByName(nombre))
             setNombre('')
@@ -25,12 +38,12 @@ const SearchBar = () => {
     return (
         <div className={st.search}>
             <form onSubmit={e => handleSubmit(e)}>
-                <input type="text" name="name" id="name" placeholder='Buscar por nombre' value={nombre} onChange={e => setNombre(e.target.value)}/>
-                {errores.nombre && <span>{errores.nombre}</span>}
+                <input type="text" name="name" id="name" placeholder='Buscar por nombre' value={nombre} onChange={handleChange} onBlur={e => handleBlur(e.target.value)}/>
                 <button type="submit">
                     <i className='bx bx-search-alt'></i>
                 </button>
             </form>
+            {errores.nombre && <span>{errores.nombre}</span>}
         </div>
     )
 };
